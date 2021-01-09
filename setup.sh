399cf734b912ea4d741c91ecacca61bd3190ad21
Nathan Lichtlé <nathan.lichtle@gmail.com>
2021-01-09 12:04:50 +0100

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 conda create --name vae python=3.7 -y
+source ~/anaconda3/etc/profile.d/conda.sh
 conda activate vae
 pip install -r requirements.txt
 export CUBLAS_WORKSPACE_CONFIG=:4096:8  # for training deterministicity
