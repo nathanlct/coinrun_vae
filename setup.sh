d8fed40c2f4add6bd1388876de3be06caf7a1d7c
Nathan Lichtlé <nathan.lichtle@gmail.com>
2021-01-14 17:58:31 +0100

    add export

--- setup.sh
+++ setup.sh
@@ -3,7 +3,10 @@
 conda create --name vae python=3.7 -y
 conda activate vae
 pip install -r requirements.txt
+
+# add to .bashrc 
 export CUBLAS_WORKSPACE_CONFIG=:4096:8  # for training deterministicity
+export PMIX_MCA_gds=^ds12  # for shutting off coinrun error
 
 cat << EOF
 Installation done in conda environment "vae".
